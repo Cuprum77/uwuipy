82f15a1b3b7e7ad186b5f74221b7bd27556174f2
Cuprum77 <blockland70@gmail.com>
2022-07-30 23:04:58 +0200

    updated setup to reflect changes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="uwuipy",
-    version="0.1.1",
+    version="0.1.2",
     author="Cuprum77",
     description="Allows the easy implementation of uwuifying words for applications like Discord bots and websites",
     long_description=long_description,
