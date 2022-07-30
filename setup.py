9cba25589c1e80e6dc16d2c70babb4ac56824e49
Cuprum77 <blockland70@gmail.com>
2022-07-30 20:55:44 +0200

    added the README to the pypi project

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="uwuipy",
-    version="0.0.3",
+    version="0.1.0",
     author="Cuprum77",
     description="Allows the easy implementation of uwuifying words for applications like Discord bots and websites",
     packages=["uwuipy"]
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
