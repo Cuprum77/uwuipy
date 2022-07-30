d7ba7a28b3a1b4949ac4f4b984018a7d1ef78687
Cuprum77 <blockland70@gmail.com>
2022-07-30 20:45:35 +0200

    update

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 import setuptools
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setuptools.setup(
     name="uwuipy",
-    version="0.0.2",
+    version="0.0.3",
     author="Cuprum77",
     description="Allows the easy implementation of uwuifying words for applications like Discord bots and websites",
     packages=["uwuipy"]
