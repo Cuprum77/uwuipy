a8417baf0e48f14fe71939c008ee1fadf827b09f
Cuprum77 <blockland70@gmail.com>
2022-07-30 21:27:11 +0200

    updated filestructure to be pypi compatible

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-import setuptools
+from setuptools import setup, find_packages
+import pathlib
 
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
-setuptools.setup(
+setup(
     name="uwuipy",
     version="0.1.0",
     author="Cuprum77",
     description="Allows the easy implementation of uwuifying words for applications like Discord bots and websites",
-    packages=["uwuipy"]
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    url="https://github.com/Cuprum77/uwuipy",
+    package_dir={"": "src"},
+    packages=find_packages(where="src")
 )
